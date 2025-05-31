190c7eb940704f38be4c3790e883f023a5b3db4c
nlankwei5 <lampteyebenezer220@gmail.com>
2025-05-31 00:14:32 +0000

    Create Serializer and update views.py for Todo list Api

--- TDL/api/views.py
+++ TDL/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from serializers import *
+from models import *
+
 
 # Create your views here.
+
+class ListToDo(generics.ListAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class UpdateToDo(generics.RetrieveUpdateAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class CreateToDo(generics.CreateAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+
+class DeleteToDo(generics.DestroyAPIView):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
