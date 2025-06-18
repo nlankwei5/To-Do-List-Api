47708c6be85449e49aa45b310dc4169b7d0411d4
nlankwei5 <lampteyebenezer220@gmail.com>
2025-06-18 12:59:52 +0000

    Add filtering, ordering, and seraching in the API

--- TDL/TDL/settings.py
+++ TDL/TDL/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt', 
+    'django_filters' 
 ]
 
 MIDDLEWARE = [
@@ -130,5 +131,7 @@ REST_FRAMEWORK = {
   ],
   'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-  ]
+  ],
+  'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'], 
+
 }
