cda9df64a4665a24542056c294d0b4d469033149
nlankwei5 <lampteyebenezer220@gmail.com>
2025-06-24 11:20:58 +0000

    Create validation for the date field
    this happens so that the user does'nt input a past date

--- TDL/api/serializers.py
+++ TDL/api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import *
+from datetime import datetime
+
 
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta :
         model = ToDo
         fields = ('id', 'title', 'description', 'date', 'completed') 
+    
+    def validate_due_date(self, value):
+        if value < datetime.today():
+             raise serializers.ValidationError("Date cannot be in the past.")
+
 
     
 
