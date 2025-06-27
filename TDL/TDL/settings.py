00433c02a199a58c55d7d98f545a19da2336333b
nlankwei5 <lampteyebenezer220@gmail.com>
2025-06-27 11:03:38 +0000

    Create env for security purposes

--- TDL/TDL/settings.py
+++ TDL/TDL/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g7hy!26wb73q02fb11^%-_a+yv*o#m+8^-#33e@_v0-bc8po4*'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
